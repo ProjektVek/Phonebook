548a1350fe2c5d34b4af7dae328f31c80cd798f3
ProjektVeK <projektvek@gmail.com>
2021-10-19 17:33:14 -0300

    Added main menu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from replit import db #Importing replit database - In replit workspace, you dont need to login
 import contacts #Importing contacts archive
+from os import system #importing function to help create the menu
 
+"""
 #Storing and retrieving data from database
 db["Harris, Steve"] = "12345666" #Defining the key value in the database
 print(db["Harris, Steve"]) #Retrieving key value in the database
@@ -9,6 +11,7 @@ print(db["Harris, Steve"]) #Retrieving key value in the database
 dictionary = {} #defining dictionary
 dictionary["Harris, Steve"] = "12345666"
 print(dictionary["Harris, Steve"])
+"""
 
 #function to get contact input
 def prompt_add_contact():
@@ -25,6 +28,32 @@ def prompt_get_contact():
         print(f"{name}'s number is {number}")
     else:
         print(f"{name}'s number not found")
+
+#Creating Menu
+main_message = 'Welcome to Phonebook\n'
+main_message += '-----------------------------\n'
+main_message += 'Please choose:\n'
+main_message += '1 - Add a new Contact\n'
+main_message += '2 - Find a Contact\n'
+main_message += '-----------------------------\n'
+
+#Defining main function
+def main():
+    print(main_message)
+    choice = input("Please make your choice: ").strip() #strip() removes any spaces at the start or end of string
+    if choice == '1':
+        prompt_add_contact()
+    elif choice == '2':
+        prompt_get_contact()
+    else:
+        print("Invalid input. Please try again.")
+
+#Execution The Program
+while True:
+    system("clear")
+    main()
+    input("Press enter to continue:")
+
 
 #Executing code
 prompt_add_contact()
